a062a8cb35529df6a30ee4c9b34c4ef1ac0f9a6f
brandon <brandon.baksh@hotmail.com>
2016-09-17 09:17:25 -0400

    Created map

--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,66 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  MapView,
+  Button
 } from 'react-native';
 
 class friendmap extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+     <View style={styles.container}>
+       <Text style={styles.welcome}>
+        friendmap
+       </Text>
+
+       <MapView
+         style={styles.map}
+         showsUserLocation={true}
+         rotateEnabled={true}
+         showCompass={true}
+       />
+     </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    margin: 5,
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 5,
+    paddingTop: 10,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  map:{
+    flex: 1,
+    height: 200,
+    margin: 10,
+  },
+  button:{
+   alignSelf: 'center',
+   marginTop: 5,
+   padding: 3,
+   borderWidth: 0.5,
+   borderColor: '#777777',
+  }
 });
 
 AppRegistry.registerComponent('friendmap', () => friendmap);
