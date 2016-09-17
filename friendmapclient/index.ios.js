b13b0a2a118396841afaa98dffa07fa1b92546fa
Laurence Li <li.laurence55@gmail.com>
2016-09-17 16:14:42 -0400

    login form update

--- friendmapclient/index.ios.js
+++ friendmapclient/index.ios.js
@@ -1,89 +1,77 @@
-  import React, { Component, LA } from 'react';
-  import { AppRegistry, Text, Image, View, StyleSheet, TextInput, ScrollView, Navigator, AlertIOS } from 'react-native';
-  import Button from 'react-native-button';
+import React, { Component } from 'react';
+import Button from 'react-native-button';
+import Hr from 'react-native-hr';
+import { AppRegistry, StyleSheet, Text, TextInput, View } from 'react-native';
 
-  class friendmapclient extends Component {
-    
-    constructor(props) {
-      super(props)
-      this.state = {
-        username: '',
-        password: ''
-      }
-    }
-    
-    _handlePress(event) {
-      let username=this.state.username;
-      let password=this.state.password;
-      sendLogin(username, password);
-    }
-    
-    render() {
-      return (
-          <View style={{padding: 10}}>
-            <TextInput
-              style={{height: 40}}
-              onChangeText={(newtext) => this.setState({username: newtext})}
-              placeholder="Username"
-              autoCorrect = {false}
-              autoCapitalize = {'none'}
-              maxLength = {16}
-            />
-            <TextInput
-              style={{height: 40}}
-              onChangeText={(newtext) => this.setState({password: newtext})}
-              placeholder="Password"
-              autoCorrect = {false}
-              autoCapitalize = {'none'}
-              maxLength = {16}
-            />
-            <Button
-              containerStyle={{padding:10, height:45, overflow:'hidden', borderRadius:4, backgroundColor: '#5BC0BE'}}
-              style={{fontSize: 20, color: '#ffffff'}}
-              onPress={() => this._handlePress()}>
-              Login
-            </Button>
-            <Button
-              containerStyle={{padding:10, height:45, overflow:'hidden', borderRadius:4, backgroundColor: '#5BC0BE'}}
-              style={{fontSize: 20, color: '#ffffff'}}>
-              Register
-            </Button>
-        </View>
-      );
-    }
+class TestProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
   }
 
-  function sendLogin(username, password){
-    console.log(username, password)
-    fetch('http://45.55.166.191:3020/test', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      })
-    })
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));   
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <Text style={styles.title}>Friendmap</Text>
+        <Text style={styles.subtitle}>Welcome</Text>
+        <Text style={styles.paragraph}>Existing users sign in below</Text>
+        <TextInput
+          style={{height: 40, top: 80, backgroundColor: "#f4f4f4", padding: 8}}
+          placeholder="Username"
+        />
+         <TextInput
+          style={{height: 40, top: 90, backgroundColor: "#f4f4f4", padding: 8}}
+          secureTextEntry={true}
+          placeholder="Password"
+        />
+        <Button
+        containerStyle={{padding:10, top: 100, height:45, overflow:'hidden', borderRadius:4, backgroundColor: '#4cdc94'}}
+        style={{fontSize: 20, color: '#ffffff'}}
+        >
+        Login
+      </Button>
+        <Text style={styles.regSubtitle}>New to Friendmap?</Text>
+        <Text style={styles.regParagraph}>Click below to register</Text>
+        <Button
+        containerStyle={{padding:10, top: 130, height:45, overflow:'hidden', borderRadius:4, backgroundColor: '#4cdc94'}}
+        style={{fontSize: 20, color: '#ffffff'}}>
+        Register
+      </Button>
+      </View>
+    );
   }
+}
 
-  const styles = StyleSheet.create({
-    bigblue: {
-      color: 'blue',
-      fontWeight: 'bold',
-      fontSize: 30,
-    },
-    red: {
-      color: 'red',
-      fontSize: 20,
-    },
-    size1: {
-      width: 400,
-      height: 400
-    }
-  });
+const styles = StyleSheet.create({
+  title: {
+    color: '#666666',
+    fontSize: 30,
+    top: 15,
+  },
+  subtitle: {
+    color: '#666666',
+    fontSize: 22,
+    top: 70,
+  },
+  paragraph: {
+    color: '#b2b2b2',
+    fontSize: 15,
+    top: 70,
+  },
+  regSubtitle: {
+    color: '#666666',
+    fontSize: 18,
+    top: 120,
+  },
+  regParagraph: {
+    color: '#b2b2b2',
+    fontSize: 15,
+    top: 120,
+  },
+  backgroundImage:{
+    flex: 1,
+    resizeMode: 'cover',
+  }
+});
 
-  AppRegistry.registerComponent('friendmapclient', () => friendmapclient);
+AppRegistry.registerComponent('TestProject', () => TestProject);
