3298cd1bc05dc8f67820721c4392f97c52a477f2
Ahmed <khanaahmed10@gmail..com>
2016-09-18 04:43:46 -0400

    markers now work but give a warning as usual

--- friendmapclient/components/mapView.js
+++ friendmapclient/components/mapView.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { View  } from 'react-native'
 import styles from '../styles.js';
 import MapView from 'react-native-maps';
+import {createMarker} from '../database'
 
 class mapView extends Component {
   state = {
@@ -19,7 +20,7 @@ class mapView extends Component {
       showsUserLocation={true}
       onPress={(event) => {
         var position = event.nativeEvent.coordinate;
-        this.setState({markers: this.state.markers.concat([])});
+        this.setState({markers: this.state.markers.concat([position])});
         console.log(this.state.markers)
         createMarker(position, this.state.id);
       }}
