abb6cd5c488f759a5702bf18d00ac7baf0fe8f5e
Ahmed <khanaahmed10@gmail..com>
2016-09-18 02:52:51 -0400

    used the database file

--- friendmapclient/index.ios.js
+++ friendmapclient/index.ios.js
@@ -4,6 +4,8 @@ import Hr from 'react-native-hr';
 import { AppRegistry, Text, Modal, TouchableHighlight, Image, View, StyleSheet, TextInput, ScrollView, Navigator, AlertIOS } from 'react-native';
 import BackgroundImage from "./BackgroundImage";
 import MapView from 'react-native-maps';
+import { createMarker } from './database';
+
 class friendmapclient extends Component {
   render() {
         return (
@@ -30,14 +32,14 @@ class mapView extends Component {
   render() {
     return (
         <View style={styles.container}>
-      
             <MapView
                 style={styles.map}
                 showsUserLocation={true}
                 onPress={(event) => {
-                    this.setState({markers: this.state.markers.concat([event.nativeEvent.coordinate])});
-                    //this.state.markers.push(event.nativeEvent.coordinate)
+                    var position = event.nativeEvent.coordinate;
+                    this.setState({markers: this.state.markers.concat([])});
                     console.log(this.state.markers)
+                    createMarker(position, this.state.id);
                 }}
             >
                 {this.state.markers.map(marker=>(
@@ -55,10 +57,12 @@ class mapView extends Component {
   }
 }
 
+function sendPlace(){
+
+}
 
 
 class loginView extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {
@@ -69,157 +73,158 @@ class loginView extends Component {
       access: 'none'
     };
   }
-  
+
    goToNext() {
       if(this.state.access == "success"){
         this.props.navigator.push({
-        name: 'mapView',
-        component: mapView
-      });
+          name: 'mapView',
+          component: mapView
+        });
       }
-     else {
-       AlertIOS.alert(
- 'Fuck you'
-);
-     }
-      
-    }
+      else {
+        AlertIOS.alert(
+          'Fuck you'
+        );
+      }
 
-  setModalVisible(visible) {
-    this.setState({
-      text: '',
-      username: '',
-      password: '',
-      modalVisible: visible
-    });
-  }
-  
-  sendLogin(username, password){
-    console.log("/////////////////////////////////", username, password)
-    fetch('http://45.55.166.191:3020/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      })
-    })
-      .then((response) => {this.setState({access: response._bodyText}); console.log("-------------------------------------------", response._bodyText); this.goToNext();})
-      .catch((error) => console.log(error))
-      .done(); 
-  
-      
-  }
-  
-  
-  
-  _handlePress(event) {
-    let username=this.state.username;
-    let password=this.state.password;
-    this.sendLogin(username, password);
-  }
+   }
 
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
+   setModalVisible(visible) {
+     this.setState({
+       text: '',
+       username: '',
+       password: '',
+       modalVisible: visible
+     });
+   }
+
+   sendLogin(username, password){
+     console.log("sendLogin, username, password - ", username, password)
+
+     fetch('http://45.55.166.191:3020/auth/signin', {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         username: username,
+         password: password,
+       })
+     })
+     .then((response) => {this.setState({access: response._bodyText}); console.log("-------------------------------------------", response._bodyText); this.goToNext();})
+     .catch((error) => console.log(error))
+     .done();
+
+
+   }
+
+
+
+   _handlePress(event) {
+     let username=this.state.username;
+     let password=this.state.password;
+     this.sendLogin(username, password);
+   }
+
+   render() {
+     let pic = {
+       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+     };
+     return (
        <BackgroundImage source={ { uri: 'https://s-media-cache-ak0.pinimg.com/originals/76/4f/86/764f863306caae67a9f1ba245bde39df.jpg'}}>
-      <View>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          >
-         <View>
-          <View style={{padding:40}}>
-            <Text style={styles.modalSubtitle}>Register</Text>
-            <TextInput
-            style={{height: 40, top: 80, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
-            placeholder="Username"
-            autoCorrect = {false}
-            autoCapitalize = {'none'}
-            maxLength = {16}
-          />
-          <TextInput
-            style={{height: 40, top: 110, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
-            secureTextEntry={true}
-            placeholder="Password"
-            autoCorrect = {false}
-            autoCapitalize = {'none'}
-            maxLength = {16}
-          />
-            <TextInput
-            style={{height: 40, top: 120, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
-            secureTextEntry={true}
-            placeholder="Confirm Password"
-            autoCorrect = {false}
-            autoCapitalize = {'none'}
-            maxLength = {16}
-          />
-            <Button
-            onPress={() => {
-              this.setModalVisible(true)
-            }}
-            containerStyle={{padding:10, top: 150, height:35, borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
-            style={{fontSize: 20, color: '#323232'}}>
-            Confirm Registration
-          </Button>
-            <Button
-            onPress={() => {
-              this.setModalVisible(false)
-            }}
-            containerStyle={{padding:10, top: 160, height:35, overflow:'hidden', borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
-            style={{fontSize: 20, color: '#b2b2b2'}}>
-            Cancel
-          </Button>
-          </View>
-         </View>
-        </Modal>
-        <View style={{padding:40, paddingTop: 100}}>
-          <Text style={styles.title}>Friendmap</Text>
-          <Text style={styles.subtitle}>Welcome</Text>
-          <TextInput
-            style={{height: 40, top: 80, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
-            placeholder="Username"
-            autoCorrect = {false}
-            autoCapitalize = {'none'}
-            maxLength = {16}
-            onChangeText={(newtext) => this.setState({username: newtext})}
-          />
-          <TextInput
-            style={{height: 40, top: 90, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
-            secureTextEntry={true}
-            placeholder="Password"
-            autoCorrect = {false}
-            autoCapitalize = {'none'}
-            maxLength = {16}
-            onChangeText={(newtext) => this.setState({password: newtext})}
-          />
-          <Button
-            containerStyle={{padding:10, top: 100, height:45, borderRadius:4, backgroundColor: '#4cdc94'}}
-            style={{fontSize: 20, color: '#ffffff'}}
-            onPress={() => this._handlePress()}
-            >
-            Login
-          </Button>
-          <Text style={styles.regSubtitle}>New to Friendmap?</Text>
-          <Button
-            onPress={() => {
-              this.setModalVisible(true)
-            }}
-            containerStyle={{padding:10, top: 200, height:35, borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
-            style={{fontSize: 20, color: 'white'}}>
-            Register
-          </Button>
-        </View>
-      </View>
-        </BackgroundImage>
-    );
-  }
+       <View>
+       <Modal
+       animationType={"slide"}
+       transparent={false}
+       visible={this.state.modalVisible}
+       >
+       <View>
+       <View style={{padding:40}}>
+       <Text style={styles.modalSubtitle}>Register</Text>
+       <TextInput
+       style={{height: 40, top: 80, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
+       placeholder="Username"
+       autoCorrect = {false}
+       autoCapitalize = {'none'}
+       maxLength = {16}
+         />
+       <TextInput
+       style={{height: 40, top: 110, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
+       secureTextEntry={true}
+       placeholder="Password"
+       autoCorrect = {false}
+       autoCapitalize = {'none'}
+       maxLength = {16}
+         />
+       <TextInput
+       style={{height: 40, top: 120, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
+       secureTextEntry={true}
+       placeholder="Confirm Password"
+       autoCorrect = {false}
+       autoCapitalize = {'none'}
+       maxLength = {16}
+         />
+       <Button
+       onPress={() => {
+         this.setModalVisible(true)
+       }}
+       containerStyle={{padding:10, top: 150, height:35, borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
+       style={{fontSize: 20, color: '#323232'}}>
+       Confirm Registration
+       </Button>
+       <Button
+       onPress={() => {
+         this.setModalVisible(false)
+       }}
+       containerStyle={{padding:10, top: 160, height:35, overflow:'hidden', borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
+       style={{fontSize: 20, color: '#b2b2b2'}}>
+       Cancel
+       </Button>
+       </View>
+       </View>
+       </Modal>
+       <View style={{padding:40, paddingTop: 100}}>
+       <Text style={styles.title}>Friendmap</Text>
+       <Text style={styles.subtitle}>Welcome</Text>
+       <TextInput
+       style={{height: 40, top: 80, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
+       placeholder="Username"
+       autoCorrect = {false}
+       autoCapitalize = {'none'}
+       maxLength = {16}
+       onChangeText={(newtext) => this.setState({username: newtext})}
+         />
+       <TextInput
+       style={{height: 40, top: 90, backgroundColor: "#f4f4f4", padding: 8, borderRadius: 3}}
+       secureTextEntry={true}
+       placeholder="Password"
+       autoCorrect = {false}
+       autoCapitalize = {'none'}
+       maxLength = {16}
+       onChangeText={(newtext) => this.setState({password: newtext})}
+         />
+       <Button
+       containerStyle={{padding:10, top: 100, height:45, borderRadius:4, backgroundColor: '#4cdc94'}}
+       style={{fontSize: 20, color: '#ffffff'}}
+       onPress={() => this._handlePress()}
+       >
+       Login
+       </Button>
+       <Text style={styles.regSubtitle}>New to Friendmap?</Text>
+       <Button
+       onPress={() => {
+         this.setModalVisible(true)
+       }}
+       containerStyle={{padding:10, top: 200, height:35, borderRadius:4, backgroundColor: 'rgba(0,0,0,0)'}}
+       style={{fontSize: 20, color: 'white'}}>
+       Register
+       </Button>
+       </View>
+       </View>
+       </BackgroundImage>
+     );
+   }
 }
 
 
@@ -304,11 +309,11 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   button:{
-   alignSelf: 'center',
-   marginTop: 5,
-   padding: 3,
-   borderWidth: 0.5,
-   borderColor: '#777777',
+    alignSelf: 'center',
+    marginTop: 5,
+    padding: 3,
+    borderWidth: 0.5,
+    borderColor: '#777777',
   }
 });
 
