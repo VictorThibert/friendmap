211f4871d7c2bb94a6029cad6e3b953c1b9675e8
Ahmed <khanaahmed10@gmail..com>
2016-10-02 02:56:40 -0400

    added readme and formated background image file

--- friendmapclient/components/BackgroundImage.js
+++ friendmapclient/components/BackgroundImage.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import { Image } from 'react-native'
 
 class BackgroundImage extends Component {
- render() {
-   const {source, children, style, ...props} = this.props
-   return (
-     <Image source={ source }
-            style={ { flex: 1, width: null, height: null, ...style } }
-            {...props}>
-       { children }
-     </Image>
-   )
- }
+  render() {
+    const {source, children, style, ...props} = this.props
+    return (
+      <Image source={ source }
+        style={ { flex: 1, width: null, height: null, ...style } }
+        {...props}>
+        { children }
+        </Image>
+    )
+  }
 }
 BackgroundImage.propTypes = {
- source: React.PropTypes.object,
- children: React.PropTypes.object,
- style: React.PropTypes.object
+  source: React.PropTypes.object,
+  children: React.PropTypes.object,
+  style: React.PropTypes.object
 }
 export default BackgroundImage
